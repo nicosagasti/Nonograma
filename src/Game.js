9f7bf4c544ef5a53b2b8404209ace4797cc413b1
nicosagasti <nicolasferminsagasti@gmail.com>
2024-04-29 18:51:46 -0300

    Fixes indentation problems + legibility

--- src/Game.js
+++ src/Game.js
@@ -37,79 +37,79 @@ function Game() {
         setColsClues(response['ColumClues']);
 
         //Inicializar las completedColumnsClues
-        for(let i = 0 ; i< response['RowClues'].length ; i++){
+        for (let i = 0; i < response['RowClues'].length; i++) {
           completedRowsClues[i] = 0;
         }
 
         //Inicializar las completedRowsClues
-        for(let i=0; i<response['ColumClues'].length; i++){
+        for (let i = 0; i < response['ColumClues'].length; i++) {
           completedColumnsClues[i] = 0;
         }
 
         procesarPreGrilla(response['Grid'], response['RowClues'], response['ColumClues']);
-      }  
+      }
     });
   }
 
-    function procesarPreGrilla(Grid, RowClues, ColumnClues){
-      let rowsLength = RowClues.length;
-      let colsLength = ColumnClues.length;
-      let diagonalLength = Math.min(rowsLength, colsLength);
+  function procesarPreGrilla(Grid, RowClues, ColumnClues) {
+    let rowsLength = RowClues.length;
+    let colsLength = ColumnClues.length;
+    let diagonalLength = Math.min(rowsLength, colsLength);
 
-      const squaresS = JSON.stringify(Grid).replaceAll('"_"', '_');
-      const rowCluesS = JSON.stringify(RowClues);
-      const colCluesS = JSON.stringify(ColumnClues);
+    const squaresS = JSON.stringify(Grid).replaceAll('"_"', '_');
+    const rowCluesS = JSON.stringify(RowClues);
+    const colCluesS = JSON.stringify(ColumnClues);
 
-      let rowAux = [0,0,0,0,0];
-      let colAux = [0,0,0,0,0];
+    let rowAux = [0, 0, 0, 0, 0];
+    let colAux = [0, 0, 0, 0, 0];
 
-      // Recorrer la diagonal de la matriz cuadrada
-      for (let i = 0; i < diagonalLength; i++) {   
-          const queryA = `checkGrid(${squaresS}, ${rowCluesS}, ${colCluesS}, [${i}, ${i}], RowSat, ColSat)`;
-          setWaiting(true);
+    // Recorrer la diagonal de la matriz cuadrada
+    for (let i = 0; i < diagonalLength; i++) {
+      const queryA = `checkGrid(${squaresS}, ${rowCluesS}, ${colCluesS}, [${i}, ${i}], RowSat, ColSat)`;
+      setWaiting(true);
+
+      pengine.query(queryA, (succes, response) => {
+        if (succes) {
+          rowAux[i] = response['RowSat'];
+          colAux[i] = response['ColSat'];
+        }
+        setWaiting(false);
+      });
+    }
+
+    // Continuar recorriendo el resto de la matriz => Verificar TODO
+    for (let i = diagonalLength; i < rowsLength; i++) {
+      for (let j = diagonalLength; j < colsLength; j++) {
+        const queryA = `checkGrid(${squaresS}, ${rowCluesS}, ${colCluesS}, [${i}, ${i}], RowSat, ColSat)`;
+        setWaiting(true);
 
         pengine.query(queryA, (succes, response) => {
-          if(succes){
+          if (succes) {
             rowAux[i] = response['RowSat'];
             colAux[i] = response['ColSat'];
           }
           setWaiting(false);
         });
+      }
     }
 
-      // Continuar recorriendo el resto de la matriz => Verificar TODO
-      for (let i = diagonalLength; i < rowsLength; i++) {
-        for (let j = diagonalLength; j < colsLength; j++) {
-         const queryA = `checkGrid(${squaresS}, ${rowCluesS}, ${colCluesS}, [${i}, ${i}], RowSat, ColSat)`;
-       setWaiting(true);
+    setCompletedRowsClues(rowAux);
+    setCompletedColumnsClues(colAux);
+  }
 
-       pengine.query(queryA, (succes, response) => {
-        if(succes){
-          rowAux[i] = response['RowSat'];
-          colAux[i] = response['ColSat'];
-        }
-        setWaiting(false);
-       });
-      }
-      }
-
-      setCompletedRowsClues(rowAux);
-      setCompletedColumnsClues(colAux);
-    }
-
-  function gameWon(RowAux,ColAux){
-    const RowAuxValues= JSON.stringify(RowAux);
-    const ColAuxValues= JSON.stringify(ColAux);
+  function gameWon(RowAux, ColAux) {
+    const RowAuxValues = JSON.stringify(RowAux);
+    const ColAuxValues = JSON.stringify(ColAux);
 
     const queryS1 = `checkWon(${RowAuxValues},${ColAuxValues},Result)`;
 
     setWaiting(true);
     pengine.query(queryS1, (success, response) => {
       if (success) {
-        setGameWonStatus(response['Result']); 
+        setGameWonStatus(response['Result']);
       }
       setWaiting(false);
-    });   
+    });
   }
 
   function handleClick(i, j) {
@@ -119,47 +119,47 @@ function Game() {
     }
 
     const content = toggleChecked ? "#" : "X";
-    
+
     // Build Prolog query to make a move and get the new satisfacion status of the relevant clues.    
     const squaresS = JSON.stringify(grid).replaceAll('"_"', '_');
-  
+
     const rowCluesS = JSON.stringify(rowsClues);
     const colCluesS = JSON.stringify(colsClues);
 
-    const queryS = `put("${content}", [${i},${j}], ${rowCluesS}, ${colCluesS},${squaresS}, ResGrid, RowSat, ColSat)`; 
-    
+    const queryS = `put("${content}", [${i},${j}], ${rowCluesS}, ${colCluesS},${squaresS}, ResGrid, RowSat, ColSat)`;
+
     setWaiting(true);
 
     pengine.query(queryS, (success, response) => {
       if (success) {
-        updateCompletedClues("row",i, response['RowSat']);
-        updateCompletedClues("col",j, response['ColSat']);
-        
+        updateCompletedClues("row", i, response['RowSat']);
+        updateCompletedClues("col", j, response['ColSat']);
+
         //Si se cumplen todas las pistas se gana el juego.
-        gameWon(completedRowsClues,completedColumnsClues);
+        gameWon(completedRowsClues, completedColumnsClues);
         setGrid(response['ResGrid']);
 
-      } 
+      }
       setWaiting(false);
     });
-    
+
   }
 
-  function updateCompletedClues(type, i, completed){
+  function updateCompletedClues(type, i, completed) {
     let cluesAux;
-    if(type === "row"){
+    if (type === "row") {
       cluesAux = [...completedRowsClues];
     }
-    else{
+    else {
       cluesAux = [...completedColumnsClues];
     }
 
     cluesAux[i] = completed;
 
-    if (type === "row"){
+    if (type === "row") {
       setCompletedRowsClues(cluesAux);
     }
-    else{
+    else {
       setCompletedColumnsClues(cluesAux);
     }
   }
@@ -168,7 +168,7 @@ function Game() {
     return null;
   }
 
-  const statusText = gameWonStatus? 'You Won!' :'Keep playing!';
+  const statusText = gameWonStatus ? 'You Won!' : 'Keep playing!';
   return (
     <div className="game">
       <Board
