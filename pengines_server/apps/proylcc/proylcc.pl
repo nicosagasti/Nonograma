4cbab99d2f1eba71ceb40371616912588405eedb
Lourdes Santillan <santillanlourdes011@gmail.com>
2024-06-07 19:24:17 -0300

    documentacion parcial de prolog

--- pengines_server/apps/proylcc/proylcc.pl
+++ pengines_server/apps/proylcc/proylcc.pl
@@ -75,7 +75,7 @@ checkClues(ClueList, GridList, Satisfied) :-
 %
 % addElement(+Element, +List, -Result)
 %
-% Adds an element to the beginning of a list
+% agrega un elemento al inicio de la lista
 addElement(X,[],[X]).
 addElement(X,[Head|Tail],R):- 
     R = [X,Head|Tail].
@@ -85,7 +85,7 @@ addElement(X,[Head|Tail],R):-
 %
 % getValuesFromColumn(+GridList, +ColumnIndex, -ColumnValues)
 %
-% Extracts the values from a specific column in a grid represented as a list of lists.
+% obtiene los valores de una columna en la grilla representada como lista de listas
 getValuesFromColumn([],_Pos,[]).
 getValuesFromColumn([H|T],Pos,List):- 
     nth0(Pos,H,Element),
@@ -166,10 +166,11 @@ checkWon([X|Xs], [Y|Ys],Res):-
     (Res1 == 0 -> Res = 0 ;  
 	(Res1 == Res2 -> Res = 1 ; Res = 0)).
 
-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
 %Obtiene una posicion de una lista.
-%Idealmente recibe: Una posicion P y una lista L.
-%Salida esperada:  La posicion P en L.
+%
+%getPos(+Pos, +List, -Return)
 getPos(0,L,R):-
 	L = [H|_T], R = H.
 getPos(N,L,R):-
@@ -177,16 +178,21 @@ getPos(N,L,R):-
 	S is N-1, 
 	getPos(S,T,R).
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
 %Lee una matriz y devuelve una columna en forma de lista.
-%Idealmente recibe: Una lista de listas (Matriz) M y una posicion P.
-%Salida esperada: Una lista con los elementos de la posicion P en M.
-listarCol([],_Pos,Lista)  :- 
-	Lista = [].  %%podria ser un hecho
+%
+% listarCol(+Grid, +Pos, -List).
+listarCol([],_Pos,[]).
+	%Lista = [].  %%podria ser un hecho
 listarCol([H|T],Pos,Lista):- 
 	nth0(Pos,H,Elemento),
     listarCol(T,Pos,ListaAux),
     addElement(Elemento,ListaAux,Lista).
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%
 generateClue([],N,Pista,Resto):- 
 	Resto = [],Pista = [N].
 generateClue([H|T],N,Pista,Resto):- 
@@ -194,26 +200,35 @@ generateClue([H|T],N,Pista,Resto):-
 	S is N+1, generateClue(T,S,Pista,Resto);
     Resto = T,Pista = [N].
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
 %Genera una lista de pistas a traves de una fila de una matriz.
-%Idealmente recibe: Una fila de una matriz.
-%Salida esperada: Una lista de pistas.
-generateCluesList([],ListaPista):- 
-	ListaPista = [],!.
+%
+%generateCluesList(+Row, -CluesList).
+generateCluesList([],[]):- !. 
+	%%ListaPista = [],!.
 generateCluesList(Fila,ListaPista):- 
 	Fila \==[], generateClue(Fila,0,Pista,Resto),
 	generateCluesList(Resto,ListaAux),
     Pista = [H], 
 	(H \== 0, addElement(H,ListaAux,ListaPista);ListaPista = ListaAux).
 
-addAtEnd(X,[],R):- 
-	R = [X].
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%Agrega un elemento al final de una lista
+%
+%addAtEnd(+Elememt, +List, -newList).
+addAtEnd(X,[],[X]).
+	%R = [X].
 addAtEnd(X,[H|T],R):- 
 	addAtEnd(X,T,Raux), 
 	R = [H|Raux].
 
-%Soluciona de una grilla dada.
-%Idealmente recibe: Pistas de filas y columnas, una grilla inicial y las dimensiones de la grilla.
-%Salida esperada: Una nueva Grilla modificada que respeta las pistas dadas.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%Resuelve una grilla dada.
+%
+%solveGrid(+RowClues, +ColClues, +Grid, -SolvedGrid, +numRows, +numCols).
 solveGrid(PistasF,PistasC,Grilla,GrillaRes,CantF,CantC):-  
     solveRows(PistasF,Grilla,GrillaAux,CantF),
     transposed(GrillaAux,GrillaTraspuesta,CantC),
@@ -223,9 +238,12 @@ solveGrid(PistasF,PistasC,Grilla,GrillaRes,CantF,CantC):-
     (Sat = true,GrillaRes = GrillaResAux2;
 	solveGrid(PistasF,PistasC,GrillaResAux2,GrillaRes,CantF,CantC)).
 
-%Verifica que cada fila correspondiente a una grilla respeta las pistas dadas.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%Verifica que cada fila de una grilla respeta las pistas dadas.
 %Idealmente recibe: Una lista de pistas, una grilla y la cantidad de filas.
 %Salida esperada: True en caso de que las filas verifiquen las pistas, false en caso contrario.
+%verifyRows(+CluesList, +Grid, +numRows, -Return ).
 verifyRows([],[],Sat,-1):- 
 	Sat = true,!.
 verifyRows([P|RestoP],[F|RestoF],Sat,N):- 
@@ -234,10 +252,13 @@ verifyRows([P|RestoP],[F|RestoF],Sat,N):-
 	P = Pista, 
 	verifyRows(RestoP,RestoF,Sat,S); 
 	Sat = false.
-                                                               
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                                                               
 %Genera la mejor respuesta posible de una grilla.
 %Idealmente recibe: Pistas de filas, una grilla y la cantidad de filas.
 %Salida esperada: Una grilla con la mayor cantidad de pistas resueltas en una iteracion.
+%solveRows(+CluesList, +Grid, -ResGrid, +numRows/Cols).
 solveRows(_PistasF,Grilla,GrillaRes,-1):-
 	GrillaRes = Grilla,!.
 solveRows(PistasF,Grilla,GrillaRes,Pos):-
@@ -250,9 +271,12 @@ solveRows(PistasF,Grilla,GrillaRes,Pos):-
     S is Pos-1,
 	solveRows(PistasF,GrillaModificada,GrillaRes,S).
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
 %Genera la grilla traspuesta de una grilla dada.
 %Idealmente recibe: Una grilla y la cantidad de filas.
 %Salida esperada: La grilla traspuesta a la dada.
+%transposed(+Grid, -transposedGrid, +numRows).
 transposed(_Matriz,MatrizTraspuesta,-1):-
 	MatrizTraspuesta = [],!.
 transposed(Matriz,MatrizTraspuesta,Pos):-
@@ -261,9 +285,12 @@ transposed(Matriz,MatrizTraspuesta,Pos):-
 	transposed(Matriz,MatrizAux,S), 
 	addAtEnd(Col,MatrizAux,MatrizTraspuesta).
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
 %Resuelve una linea dada de la forma mas completa posible.
 %Idealmente recibe: Una lista de posibles combinaciones validas y la longitud de la linea.
 %Salida esperada: Una linea con la mayor cantidad de casilleros resueltos.
+%solveLine(+PosibilitiesList, -solvedLine, +lineLength)
 solveLine(_ListaPosibilidades,LineaResuelta,-1):- 
 	LineaResuelta = [],!.
 solveLine(ListaPosibilidades,LineaResuelta,Long):-
@@ -277,15 +304,21 @@ solveLine(ListaPosibilidades,LineaResuelta,Long):-
     append(LineaAux,["X"],LineaResuelta);
     append(LineaAux,[_],LineaResuelta)).
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
 %Genera una lista con las posibilidades validas de una Fila.
 %Idealmente recibe: Una fila y su pista asociada.
 %Salida esperada: Una lista con las diferentes posibilidades validas de la fila.
+%generatePosibilitiesList(+Row, +rowClue, -posibilitiesList)
 generatePosibilitiesList(Fila,Pista,ListaPosibilidades):-
 	findall(Fila,generatePosibility(Fila,Pista),ListaPosibilidades).
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
 %Genera las posibles combinaciones de una fila.
 %Idealmente recibe: Una fila y una pista.
 %Salida esperada: Distintas combinaciones validas de la fila dada.
+%generatePosibility(+Row, +rowClue, -Return)
 generatePosibility([],[]):-!.
 generatePosibility(Fila,[PistaActual|Resto]):- 
 	espaciar(Fila,FilaAux),
@@ -294,6 +327,8 @@ generatePosibility(Fila,[PistaActual|Resto]):-
 	Resto==[], espaciar(FilaConPista,FilaConPistayEspacio)),
     generatePosibility(FilaConPistayEspacio,Resto).
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
 %Los siguientes tres predicados se encargan de generar distintas combinaciones dentro de la fila dada.
 %Agrega a la fila un secuencia de '#' correspondientes a la pista.
 addClue(Linea,Linea, 0).
@@ -302,43 +337,63 @@ addClue(["#"|Linea], RestoLinea, N):-
 	S is N - 1,
     addClue(Linea, RestoLinea, S).
 
-%Genera dentro de nuestra fila un serie de espacios indeterminados.
+%Genera espacios dentro la fila.
 espaciar(Linea, Linea).
 espaciar(["X"|Linea],RestoLinea) :- 
 	espaciar(Linea, RestoLinea).
 
-%Agrega un espacio individual dentro de nuestra fila luego de haber agregado una pista.
+%Agrega un espacio individual dentro de la fila luego de haber agregado una pista.
 agregarEspacio(["X"|T],T).
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
 %Determina la longitud de una lista.
-%Idealmente recibe: Una lista.
-%Salida esperada: La longitud de una lista.
+%
+%longitud(+List,-Length).
 longitud([],R):- 
 	R is -1.
 longitud([_H|T],R):- 
 	longitud(T,Raux), 
 	R is Raux + 1.
 
-% Metodo utilizado para inicializar pistas: %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% Predicado para inicializar pistas: %
 % Predicado principal
+%
+%compareGrid(+Grid, +SolvedGrid, -RowsClues, -ColumnsClues, +CantCol)
 compareGrid(Grid, SolvedGrid, RowsClues, ColumnsClues, CantCol) :-
     compareRows(Grid, SolvedGrid, RowsClues),
     transposed(Grid, TransposedGrid, CantCol),
     transposed(SolvedGrid, TransposedSolvedGrid, CantCol),
     compareRows(TransposedGrid, TransposedSolvedGrid, ColumnsClues).
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
 % Comparar filas
+%
+%compareRows(+Row1, +Row2, +CluesList).
 compareRows([], [], []).
 compareRows([Row1|Rest1], [Row2|Rest2], [Clue|CluesRest]) :-
     (rows_equal(Row1, Row2) -> Clue = 1 ; Clue = 0),
     compareRows(Rest1, Rest2, CluesRest).
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
 % Comparar dos filas considerando "X" y "_" como equivalentes
+%
+%rows_equal(+row1,+row2).
 rows_equal([], []).
 rows_equal([H1|T1], [H2|T2]) :-
     equal_cells(H1, H2),
     rows_equal(T1, T2).
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
 % Comparar dos celdas considerando "X" y "_" como equivalentes
+%
+%equal_cells(+cell1, +cell2).
 equal_cells(X, Y):- X == "#", Y =="#", !.
 equal_cells(X, Y) :- X \== "#", Y\=="#".
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
