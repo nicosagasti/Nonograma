c7815e1f2502ff043669f8beafbaa9e20c5f3dec
nicosagasti <nicolasferminsagasti@gmail.com>
2024-06-10 12:01:37 -0300

    Update init.pl

--- pengines_server/apps/proylcc/init.pl
+++ pengines_server/apps/proylcc/init.pl
@@ -5,17 +5,16 @@
  * Predicate specifying the initial grid, which will be shown at the beginning of the game,
  * including the rows and columns clues.
  */
-	init(
-		[[3], [1,2], [4], [5], [5]],	% RowsClues
-		
-		[[2], [5], [1,3], [5], [4]], 	% ColsClues
-		
-		[["X", _ , _ , _ , _ ], 		
-		 ["X", _ ,"X", _ , _ ],
-		 ["X", _ , _ , _ , _ ],		% Grid
-		 ["#","#","#", _ , _ ],
-		 [ _ , _ ,"#","#","#"]
-		]
-		).
 
+init(
+[[3], [1,2], [4], [5], [5]],	% RowsClues
 
+[[2], [5], [1,3], [5], [4]], 	% ColsClues
+
+[["X", _ , _ , _ , _ ], 		
+ ["X", _ ,"X", _ , _ ],
+ ["X", _ , _ , _ , _ ],		% Grid
+ ["#","#","#", _ , _ ],
+ [ _ , _ ,"#","#","#"]
+]
+).
